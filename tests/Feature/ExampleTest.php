95d9ef105cecce2f574f1d93ecf710205d794a62
Mo Khosh <mskhoshnazar@gmail.com>
2024-06-15 19:18:08 +0330

    test basic user resource

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,26 @@
 <?php
 
-it('returns a successful response', function () {
-    $response = $this->get('/');
+use App\Filament\Resources\UserResource;
+use App\Filament\Resources\UserResource\Pages\ListUsers;
+use App\Models\User;
 
-    $response->assertStatus(200);
+use function Pest\Laravel\be;
+use function Pest\Laravel\get;
+use function Pest\Livewire\livewire;
+
+beforeEach(function () {
+    be($this->user = User::factory()->create());
+});
+
+it('will load', function () {
+    get(UserResource::getUrl())->assertSuccessful();
+});
+
+it('shows users', function () {
+    $users = User::factory(5)->create();
+
+    livewire(ListUsers::class)
+        ->assertSuccessful()
+        ->assertCanSeeTableRecords($users)
+        ->assertSee($users->first()->name);
 });
